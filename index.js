0d83040ad421c78bcc78c0f02583a5d282fbcfa7
Danny <barakadan421@gmail.com>
2023-08-09 22:27:36 +0300

    Fibonacci

---
+++ index.js
@@ -0,0 +1,28 @@
+//* Math Algorithms
+// !1- Fibonacci sequence **************
+// Problem: -Give a number 'n', find the first 'n' elements of the Fibonacci sequence.
+// In mathematics, the Fibonacci sequence is a sequence in which each number is the sum of the two preceding ones.
+// The first two numbers in the sequence are 0 and 1.
+// Note: the first two numbers in the sequence must be 0 and 1.
+
+// ======= Solution =======
+// function fibonacci(n) {
+//   const fib = [0,1]
+//   for(let i=2; i<n; i++) {
+//       fib[i] = fib[i-1] + fib[i-2]
+//   }
+//   return fib;
+// }
+
+// console.log(fibonacci(2)) // [0,1]
+// console.log(fibonacci(3)) // [0,1,1]
+// console.log(fibonacci(7)) // [0,1,1,2,3,5,8]
+
+// >>>>>>>>> Big O Notation = O(n)
+
+// !2- Factorial of a number
+// !3- Prime number
+// !4- Power of a two
+// !5- Recursion
+// !6- Fibonacci sequence with recursion
+// !7- Factorial of a number with recursion
